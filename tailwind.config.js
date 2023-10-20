34c34125349d55d85766565dcf8adaccf17e08cb
pm-ihes <pascal.misler@ihes.io>
2023-10-20 22:52:03 +0200

    Fixing Screen Bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      '-sm': {'max': '700px'},
+      'sm': '701px',
+      '-md': {'max': '1000px'},
+      'md': '1001px',
+      '-lg': {'max': '1500px'},
+      'lg': '1501px'
+    },
   },
   plugins: [],
   important: true,

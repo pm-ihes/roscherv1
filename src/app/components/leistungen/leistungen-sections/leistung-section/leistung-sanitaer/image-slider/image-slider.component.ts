902d23ff31afe4c431ec85558cf2da33fc8c171c
pm-ihes <pascal.misler@ihes.io>
2023-12-09 01:05:47 +0100

    WIP: mobile layout for image slider

--- src/app/components/leistungen/leistungen-sections/leistung-section/leistung-sanitaer/image-slider/image-slider.component.ts
+++ src/app/components/leistungen/leistungen-sections/leistung-section/leistung-sanitaer/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { Component, ElementRef, HostListener, QueryList, Renderer2, ViewChildren } from '@angular/core';
 import Aos from 'aos';
 
 @Component({
@@ -10,53 +10,79 @@ export class ImageSliderComponent {
 
   cardElements?: ElementRef[];
   thamElements?: ElementRef[];
+  mobileElements?: ElementRef[];
 
   viewLoaded: boolean = false;
   menuOpened: boolean = false;
+  mMenuOpened: boolean[] = [false, false, false, false, false];
+  isMobile: boolean = false;
 
   cards = [
     {
       image: 'assets/images/image_team.jpg',
-      text: ''
+      text: '',
+      notes: ['Armaturen','Waschbecken', 'Waschtisch']
     },
     {
       image: 'assets/images/image_team.jpg',
-      text: ''
+      text: '',
+      notes: ['Duschen','Offene Duschen', 'Duschwannen']
     },
     {
       image: 'assets/images/image_team.jpg',
-      text: ''
+      text: '',
+      notes: ['WCs','Urinale', 'Bidets', 'Dusch-WCs']
     },
     {
       image: 'assets/images/image_team.jpg',
-      text: ''
+      text: '',
+      notes: ['Freistehende Badewanne','Eckbadewanne', 'Whirlpool']
     },
     {
       image: 'assets/images/image_team.jpg',
-      text: ''
+      text: '',
+      notes: ['Installationselemente','Halterungen']
     }
   ];
 
   constructor(private renderer: Renderer2, private elem: ElementRef) { }
 
+
+  ngOnInit() {
+    this.isMobile = this.getSize();
+  }
+
+
   ngAfterViewInit() {
 
     Aos.refresh();
 
-    this.cardElements = this.elem.nativeElement.querySelectorAll('.card');
-    this.thamElements = this.elem.nativeElement.querySelectorAll('.tham');
-
+    this.refreshElements();
 
     if (this.cardElements) {
-      this.cardElements.forEach(card => {
+      this.cardElements.forEach((card) => {
         this.renderer.removeClass(card, 'active');
       })
 
-      this.renderer.addClass(this.cardElements[0], 'active');
+      if (!this.isMobile){
+        this.renderer.addClass(this.cardElements[0], 'active');
+      }
     }
-
-
   }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = this.getSize();
+    this.refreshElements();
+  }
+
+
+  refreshElements(){
+    this.cardElements = this.elem.nativeElement.querySelectorAll('.card');
+    this.thamElements = this.elem.nativeElement.querySelectorAll('.tham');
+    this.mobileElements = this.elem.nativeElement.querySelectorAll('.mobile-notes');
+  }
+
 
   addActive(event: Event) {
     const target = event.target as HTMLElement;
@@ -64,7 +90,7 @@ export class ImageSliderComponent {
     this.menuOpened = false;
 
     if (this.cardElements && target) {
-      this.cardElements.forEach(card => {
+      this.cardElements.forEach((card) => {
         this.renderer.removeClass(card, 'active');
       })
 
@@ -78,12 +104,41 @@ export class ImageSliderComponent {
     }
   }
 
-  toggleMenu(event: Event) {
+
+  toggleMenu(event: Event, index: number = -1) {
     const tham = event.currentTarget as HTMLElement;
+
+    console.log(index);
 
     this.menuOpened = !this.menuOpened;
 
     tham.classList.toggle('tham-active');
+
+    if (this.isMobile && this.mobileElements) {
+      this.mMenuOpened[index] = !this.mMenuOpened[index];
+
+      var mobileEl = this.mobileElements[index];
+      console.log(mobileEl);
+
+      for (let i = 0; i < 5; i++) {
+        if (!this.mMenuOpened[i]) {
+          this.renderer.removeClass(this.mobileElements[i], 'active');
+        }
+      }
+
+      if (this.mMenuOpened[index]){
+        this.renderer.addClass(this.mobileElements[index], 'active');
+      }
+    }
+  }
+
+
+  getSize() {
+    if (window.innerWidth < 1001) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }

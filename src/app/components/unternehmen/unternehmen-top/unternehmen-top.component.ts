a806e8c9ab2e008517571c488e773ee4ae7d33f5
pm-ihes <pascal.misler@ihes.io>
2024-01-25 23:52:02 +0100

    WIP: Added Partner Overview

--- src/app/components/unternehmen/unternehmen-top/unternehmen-top.component.ts
+++ src/app/components/unternehmen/unternehmen-top/unternehmen-top.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
-  selector: 'app-unternehmen-top',
-  templateUrl: './unternehmen-top.component.html',
-  styleUrls: ['./unternehmen-top.component.css']
+    selector: 'app-unternehmen-top',
+    templateUrl: './unternehmen-top.component.html',
+    styleUrls: ['./unternehmen-top.component.css']
 })
 export class UnternehmenTopComponent {
+    @ViewChild('.heading_unternehmen')
+    heading?: ElementRef;
 
+    ngOnInit(): void {
+        this.initGsap();
+    }
+
+    initGsap() {
+        gsap.registerPlugin(ScrollTrigger);
+
+        //ScrollTrigger: Heading MainPage
+        let tl_heading = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.heading_unternehmen',
+                start: `top +=${this.getOffsetTop}px`,
+                end: 'bottom top',
+                scrub: 1
+            }
+        });
+
+        tl_heading.to('.heading_unternehmen', {
+            translateY: '-40%'
+        });
+    }
+
+    getOffsetTop() {
+        return this.heading?.nativeElement.getBoudingClientRect().top;
+    }
 }
